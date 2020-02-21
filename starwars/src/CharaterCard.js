e9cb2177e56cb005c4de5a7a0200dbfa491aad56
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-21 12:12:57 -0500

    put them into cards and styled them with styled components

--- starwars/src/CharaterCard.js
+++ starwars/src/CharaterCard.js
@@ -1,16 +1,47 @@
 import React from 'react';
 import styled from 'styled-components';
 
-
+const CharacterContainer = styled.div`
+color: white;
+font-size: 2rem;
+text-shadow: -1.5px -1.5px 0 #000, 1.5px 1.5px 0 #000, 0 -1.5px 0 #000,
+0 1.5px 0 #000, 1.5px -1.5px 0 #000, -1.5px 1.5px 0 #000, 1.5px 0 0 #000,
+-1.5px 0 0 #000; 
+line-height:4rem;
+border: 3px solid black;
+width:35%;
+letter-spacing: .5rem;
+border-radius: 10px;
+display:flex;
+flex-wrap:wrap;
+justify-content: space-around;
+align-items: center;
+`
+const NameTitle = styled.h1`
+font-size: 2.5rem;
+padding:1rem;
+padding-top:0rem;
+margin-bottom: 0rem;
+`
+const BirthDiv = styled.div`
+padding: 1rem;
+`
+const HeightDiv = styled.div`
+border-top: black dashed 2px;
+padding: .5rem;
+`
+const WeightDiv = styled.div`
+border-bottom:black dashed 2px;
+padding: .5rem;
+`
 
 
 export default function CharacterCard({name, height, mass, birth_year}){
     return (
-    <div>
-        <h1>{name}</h1>
-        <div>{height}inches, {mass}pounds</div>
-        <div>{birth_year}</div>
-
-
-    </div>)
+    <CharacterContainer>
+        <NameTitle>{name}</NameTitle>
+        <HeightDiv>Height: {height} inches</HeightDiv>
+        <WeightDiv>Weight: {mass} pounds </WeightDiv>
+        <BirthDiv>Born: {birth_year}</BirthDiv>
+    </CharacterContainer>)
 }
